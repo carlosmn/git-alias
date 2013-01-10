aa2f41047ad38a7ea3638ded9903211bbd75fbfd
Carlos Martín Nieto <cmn@dwim.me>
2013-01-11 00:16:31 +0100

    Allow listing one alias and setting

--- Main.cs
+++ Main.cs
@@ -7,12 +7,13 @@ namespace gitalias
 {
 	class MainClass
 	{
+		static readonly string prefix = "alias.";
+
 		public static void List(Configuration cfg)
 		{
 			var aliases = new List<KeyValuePair<string,string>>();
-			var prefix = "alias.";
 
-			foreach (var entry in cfg) {
+			foreach (var entry in cfg.OfType<ConfigurationEntry<string>>()) {
 				if (entry.Key.StartsWith(prefix)) {
 					var pair = new KeyValuePair<string, string>(entry.Key.Substring(prefix.Length), entry.Value);
 					aliases.Add(pair);
@@ -25,6 +26,20 @@ namespace gitalias
 				Console.WriteLine("{0} = {1}", entry.Key.PadRight(width), entry.Value);
 		}
 
+		public static void ListOne(Configuration cfg, string name)
+		{
+			var alias= cfg.Get<string>(String.Concat(prefix, name), null);
+			if (alias == null)
+				Console.Error.WriteLine("No such alias '{0}'", name);
+			else
+				Console.WriteLine("{0} = {1}", name, alias);
+		}
+
+		public static void Set(Configuration allcfg, string name, string value)
+		{
+			allcfg.Set<string>(String.Concat(prefix, name), value, ConfigurationLevel.Global);
+		}
+
 		public static void Main (string[] args)
 		{
 			var repo = new Repository(".");
@@ -33,6 +48,12 @@ namespace gitalias
 			switch (args.Length) {
 			case 0:
 				List(cfg);
+				break;
+			case 1:
+				ListOne(cfg, args[0]);
+				break;
+			case 2:
+				Set(cfg, args[0], args[1]);
 				break;
 			}
 		}
